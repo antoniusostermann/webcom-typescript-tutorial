74aafab83848a55a1134c6240e4606f21098745b
Thilo Leise <tl@web-computing.de>
2018-03-02 14:55:27 +0100

    person with optional parameter, alternative MethodCall, strict type

--- app.ts
+++ app.ts
@@ -6,11 +6,18 @@ interface PriceWithTaxCalculatable {
     calcPriceWithTax(): number;
 }
 
+interface Person {
+    firstName: string;
+    lastName: string;
+    purchasedProducts?: number;
+}
+
 class Product implements Tagable {
     getTags(): string[] {
         return this.tags;
     }
     private displayName: string;
+    private buyer: Person[] = [];
     constructor(public title: string, public subTitle: string, public price: number, public tags: string[]){
         this.displayName = title + " - " + subTitle
     }
@@ -21,6 +28,17 @@ class Product implements Tagable {
         const tax = this.price * 0.19;
         return this.price + tax;
     }
+    addToBuyer(person: Person) {
+        if(person.purchasedProducts) {
+            person.purchasedProducts++;
+        } else {
+            person.purchasedProducts = 1;
+        }
+        this.buyer.push(person);
+    }
+    getBuyer(): Person[] {
+        return this.buyer;
+    }
 }
 
 let echoDot: Product = new Product("EchoDot", "Alles was Sie an Alexa lieben", 100, ["AssistantJS", "Alexa", "echo"]);
@@ -30,3 +48,13 @@ console.log(echoDot.getTags().filter((tag: string) => tag.startsWith("A")));
 
 let priceWithTaxCalculatable: PriceWithTaxCalculatable = echoDot;
 console.log(priceWithTaxCalculatable.calcPriceWithTax());
+
+let thilo: Person = {
+    firstName: "Thilo",
+    lastName: "Leise",    
+}
+
+echoDot.addToBuyer(thilo);
+echoDot.addToBuyer({firstName: "Jan", lastName: "Trotnow"});
+
+console.log(echoDot.getBuyer());
