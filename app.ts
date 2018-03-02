ed2ea235ab577cfd6d08bf9e206ec1ee2ad0babd
Thilo Leise <tl@web-computing.de>
2018-03-02 12:37:41 +0100

    add tax

--- app.ts
+++ app.ts
@@ -1,12 +1,17 @@
 class Product {
     private displayName: string;
-    constructor(public title: string, public subTitle: string){
+    constructor(public title: string, public subTitle: string, public price){
         this.displayName = title + " - " + subTitle
     }
     toString(){
         return this.displayName;
     }
+    calcPriceWithTax() {
+        const tax = this.price * 0.19;
+        return this.price + tax;
+    }
 }
 
-let echoDot: Product = new Product("EchoDot", "Alles was Sie an Alexa lieben");
+let echoDot: Product = new Product("EchoDot", "Alles was Sie an Alexa lieben", "100");
 console.log(echoDot.toString());
+console.log(echoDot.calcPriceWithTax());
