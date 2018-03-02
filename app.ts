850b5d143d1e7b6bcaadb72c994439612df00c32
Thilo Leise <tl@web-computing.de>
2018-03-02 12:40:53 +0100

    add types for taxCalculation

--- app.ts
+++ app.ts
@@ -1,17 +1,17 @@
 class Product {
     private displayName: string;
-    constructor(public title: string, public subTitle: string, public price){
+    constructor(public title: string, public subTitle: string, public price: number){
         this.displayName = title + " - " + subTitle
     }
     toString(){
         return this.displayName;
     }
-    calcPriceWithTax() {
+    calcPriceWithTax(): number {
         const tax = this.price * 0.19;
         return this.price + tax;
     }
 }
 
-let echoDot: Product = new Product("EchoDot", "Alles was Sie an Alexa lieben", "100");
+let echoDot: Product = new Product("EchoDot", "Alles was Sie an Alexa lieben", 100);
 console.log(echoDot.toString());
 console.log(echoDot.calcPriceWithTax());
